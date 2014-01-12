b03416ac5eafdb368e700f0176217820eab1d4e6
ILWT <tsubus@lagat.org>
2014-01-12 14:00:37 +0100

    Add Fast Charge QS Tile [1/2]
    Change-Id: I9e28bb14549c176cacfe2a861b1e5f56cd9e242f
    
    Conflicts:
    	core/java/com/android/internal/util/slim/DeviceUtils.java
    	core/java/com/android/internal/util/slim/QSConstants.java
    	core/res/res/values/config.xml
    	packages/SystemUI/res/values/custom_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
    
    Conflicts:
    	core/java/com/android/internal/util/liquid/DeviceUtils.java
    	core/res/res/values/config.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
    
    Conflicts:
    	core/java/com/android/internal/util/liquid/DeviceUtils.java
    	core/java/com/android/internal/util/liquid/QSConstants.java
    	packages/SystemUI/res/values/liquid_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
    
    Conflicts:
    	packages/SystemUI/res/values/liquid_strings.xml
    
    Conflicts:
    	core/java/com/android/internal/util/liquid/QSConstants.java
    	packages/SystemUI/res/values/liquid_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java

--- core/java/com/android/internal/util/liquid/QSConstants.java
+++ core/java/com/android/internal/util/liquid/QSConstants.java
@@ -60,6 +60,7 @@ public class QSConstants {
         public static final String TILE_CUSTOM_KEY = " Key=";
         public static final String TILE_CUSTOM_DELIMITER = " =action= ";
         public static final String TILE_ONTHEGO = "toggleOnTheGo";
+        public static final String TILE_FCHARGE = "toggleFastCharge";
         public static final String TILE_DELIMITER = "|";
         public static ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
 

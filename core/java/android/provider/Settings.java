b46746dff2a3adb637143586a62432e664947852
hany <hany.alsamman@gmail.com>
2014-02-24 03:19:35 +0000

    XuiMod: Toast Animations [1/2]
    Add Xylon, Xperia Z1, TokoROM Transition and more
    
    https://github.com/zst123/XuiMod/commit/8fc1152fb485ed5e007ac8d4ea9ad7b7f1c493d8
    
    https://github.com/zst123/XuiMod/commit/1989a90850e5491799620e1f86fcbc9ae54f5161
    
    https://github.com/zst123/XuiMod/commit/e3383f127244822b9fc61db169b59e4a502d127b
    
    Credit zst123, thanks for GwonHyeok
    
    Signed-off-by: hany <hany.alsamman@gmail.com>
    
    Conflicts:
    	core/res/res/values/symbols.xml
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/pa_symbols.xml
    
    Conflicts:
    	core/res/res/values/liquid_symbols.xml
    
    Conflicts:
    	core/res/res/values/liquid_symbols.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4302,6 +4302,12 @@ public final class Settings {
 		public static final String PROGRESSBAR_COLOR_4 = "progressbar_color_4";
 
         /**
+         * Toast Animations
+         * @hide
+         */
+        public static final String TOAST_ANIMATION = "toast_animation";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

68dedc0e88bf85a9f902297c6642f726d5fc1554
liquid0624 <rjbirnesser@gmail.com>
2014-03-03 07:07:10 -0500

    Frameworks: status bar notification count (1/2)
    credit: burnsra & rmcc (cm)
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    
    Conflicts:
    	packages/SystemUI/res/values/dimens.xml
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4677,6 +4677,12 @@ public final class Settings {
          */
         public static final String STATUSBAR_CLOCK_FONT_STYLE = "statusbar_clock_font_style";
 
+		/**
+		 * Show the pending notification counts as overlays on the status bar
+		 * @hide
+		 */
+		public static final String STATUS_BAR_NOTIFICATION_COUNT = "status_bar_notification_count";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.

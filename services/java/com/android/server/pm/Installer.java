e2caa1c235294aacdf493ab7e9440a6877544711
Robert Craig <rpcraig@tycho.ncsc.mil>
2014-03-04 11:57:23 -0500

    Allow PMS to restorecon directories under /data.
    This change applies a relabel to both /data/data and
    /data/user directories on boot. Not every boot will
    apply this relabeling however. The appropriate
    seapp_contexts is hashed and compared to
    /data/system/seapp_hash to decide if the relabel
    should occur.
    
    Change-Id: I05e8b438950ddb908e46c9168ea6ee601e6d674f
    Signed-off-by: rpcraig <rpcraig@tycho.ncsc.mil>

--- services/java/com/android/server/pm/Installer.java
+++ services/java/com/android/server/pm/Installer.java
@@ -562,4 +562,8 @@ public final class Installer {
 
         return execute(builder.toString());
     }
+
+    public boolean restoreconData() {
+        return (execute("restorecondata") == 0);
+    }
 }

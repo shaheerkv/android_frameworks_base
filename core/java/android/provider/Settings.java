120b68db60483dce676897459a358c4f16d1bb41
Jmz <jmztaylor@gmail.com>
2014-03-09 08:28:00 +0100

    FWB: Allow disabling of FC dialogs (1/2)
    Change-Id: If87aa74ebb45849f6e5fe3a53ce9beeaac28f624
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	services/java/com/android/server/am/ActivityManagerService.java
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4833,6 +4833,12 @@ public final class Settings {
         public static final String POWER_MENU_USER_ENABLED = "power_menu_user_enabled";
 
         /**
+         * Disable FC Notifications
+         * @hide
+         */
+        public static final String DISABLE_FC_NOTIFICATIONS = "disable_fc_notifications";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

9edb57665b88fd2dfbaf25dc921e7dfd2f77251c
Evisceration <eviscerationls@gmail.com>
2014-03-13 10:10:34 +0100

    On-The-Go mode [1/2]
    On-The-Go turns on the camera of your phone and streams it to an overlay,
    which is drawn over everything.
    this makes you feel like your display is transparent, as you see the content
    behind it.
    
    its really useful when you are using your phone while walking, thats why
    we called it "On-The-Go"
    
    allows to change transparency of the overlay via the notification.
    
    have fun!
    
    Change-Id: I3efccb5fac41a946f6cb4992e9181422980ee5e4
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/custom_symbols.xml
    	core/res/res/values/ose_strings.xml
    	packages/SystemUI/res/values/custom_strings.xml
    	policy/src/com/android/internal/policy/impl/GlobalActions.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/liquid_symbols.xml
    	packages/SystemUI/AndroidManifest.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1022,7 +1022,7 @@ public final class Settings {
          * The content:// style URL for this table
          */
         public static final Uri CONTENT_URI =
-            Uri.parse("content://" + AUTHORITY + "/system");
+                Uri.parse("content://" + AUTHORITY + "/system");
 
         private static final NameValueCache sNameValueCache = new NameValueCache(
                 SYS_PROP_SETTING_VERSION,
@@ -1194,7 +1194,7 @@ public final class Settings {
         public static Uri getUriFor(String name) {
             if (MOVED_TO_SECURE.contains(name)) {
                 Log.w(TAG, "Setting " + name + " has moved from android.provider.Settings.System"
-                    + " to android.provider.Settings.Secure, returning Secure URI.");
+                       + " to android.provider.Settings.Secure, returning Secure URI.");
                 return Secure.getUriFor(Secure.CONTENT_URI, name);
             }
             if (MOVED_TO_GLOBAL.contains(name) || MOVED_TO_SECURE_THEN_GLOBAL.contains(name)) {
@@ -1560,7 +1560,7 @@ public final class Settings {
 
         /** @hide */
         public static boolean hasInterestingConfigurationChanges(int changes) {
-            return (changes&ActivityInfo.CONFIG_FONT_SCALE) != 0;
+            return (changes & ActivityInfo.CONFIG_FONT_SCALE) != 0;
         }
 
         /** @deprecated - Do not use */
@@ -4313,7 +4313,30 @@ public final class Settings {
          */
         public static final String VIBRATION_MULTIPLIER = "vibration_multiplier";
 
-	    /**
+        /**
+         * If On-The-Go should be displayed at the power menu.
+         *
+         * @hide
+         */
+        public static final String POWER_MENU_ONTHEGO_ENABLED = "power_menu_onthego_enabled";
+
+        /**
+         * The alpha value of the On-The-Go overlay.
+         *
+         * @hide
+         */
+        public static final String ON_THE_GO_ALPHA = "on_the_go_alpha";
+
+        /**
+         * The camera instance to use.
+         * 0 = Rear Camera
+         * 1 = Front Camera
+         *
+         * @hide
+         */
+        public static final String ON_THE_GO_CAMERA = "on_the_go_camera";
+
+        /**
          * Ram Usage Bar recent switcher mode
          * @hide
          */

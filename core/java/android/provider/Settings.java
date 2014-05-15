cec9ac66e013710b9b65afe09aa384939638857c
Raj Yengisetty <rajesh@cyngn.com>
2014-05-15 15:33:00 -0700

    Protected App [1/3]  PackageManager:    Add support for Protected App Components    Adding protect flag to ApplicationInfo
     SettingsProvider:
       New DB entry for protected app components
    
    Change-Id: If04843e93b572191d4654c01f4da8f087ba9fba8
    
    Conflicts:
    	core/java/android/content/pm/ApplicationInfo.java
    
    Conflicts:
    	core/java/android/content/pm/ApplicationInfo.java
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6272,6 +6272,11 @@ public final class Settings {
         public static final String ADVANCED_REBOOT = "advanced_reboot";
 
         /**
+         * Protected Components
+         */
+        public static final String PROTECTED_COMPONENTS = "protected_components";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear

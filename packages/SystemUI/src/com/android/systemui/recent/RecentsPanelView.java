c4e0c7f86e9d7d14cd66a7243c42d67a688e0dca
PrimeDirective <activethrasher00@gmail.com>
2014-05-29 23:35:30 -0400

    RecentsPanelView: remove internal getter for isHighEndGfx()
    if we're going to store this as a global we might as well set it in the construct
    instead of looking for it every time the recents are inflated
    
    Change-Id: Ic38e98e5cf2ae399d51b63089d7cb2ab3ac97f51

--- packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
@@ -306,6 +306,8 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
         mRecentTasksLoader = RecentTasksLoader.getInstance(context);
         mRecentsActivity = (RecentsActivity) context;
         a.recycle();
+
+        mHighEndGfx = ActivityManager.isHighEndGfx();
     }
 
     public int numItemsInOneScreenful() {
@@ -540,7 +542,6 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
         });
 
         if (mRecentsScrim != null) {
-            mHighEndGfx = ActivityManager.isHighEndGfx();
             if (!mHighEndGfx) {
                 mRecentsScrim.setBackground(null);
             } else if (mRecentsScrim.getBackground() instanceof BitmapDrawable) {

47caa9bf6ba75e142435060a7b5c58a2fd0bc9da
BigBrother1984 <stevewatersy@gmail.com>
2014-06-13 21:22:53 +0200

    VolumePanel: Fix Theme Engine support with On-The-Spot dialog
    Our on the spot feature needs to write to system. Since VolumePanel when is
    recreated uses theme engine context (and in this case is fked), pass to on the spot dialog always the
    first system initialized one. Thanks to @David96 helping find this out :)
    
    This way we lose theming for the safe headset on the spot dialog,
    but we're already finding a really good fix, we have something already :)
    
    Fix AOSPA-482
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    Change-Id: I492cd8e24b09d856d23697b84113664379703cf1
    Conflicts:
    	core/java/android/view/VolumePanel.java

--- media/java/android/media/AudioService.java
+++ media/java/android/media/AudioService.java
@@ -497,7 +497,7 @@ public class AudioService extends IAudioService.Stub {
         Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
         mHasVibrator = vibrator == null ? false : vibrator.hasVibrator();
 
-       // Intialized volume
+        // Intialized volume
         MAX_STREAM_VOLUME[AudioSystem.STREAM_VOICE_CALL] = SystemProperties.getInt(
             "ro.config.vc_call_vol_steps",
            MAX_STREAM_VOLUME[AudioSystem.STREAM_VOICE_CALL]);
@@ -510,7 +510,7 @@ public class AudioService extends IAudioService.Stub {
         sSoundEffectVolumeDb = context.getResources().getInteger(
                 com.android.internal.R.integer.config_soundEffectVolumeDb);
 
-        mVolumePanel = new VolumePanel(context, this);
+        mVolumePanel = new VolumePanel(context, null, this); // first created panel
         mForcedUseForComm = AudioSystem.FORCE_NONE;
 
         createAudioSystemThread();
@@ -4597,7 +4597,7 @@ public class AudioService extends IAudioService.Stub {
                     }
 
                     final Context context = mUiContext != null ? mUiContext : mContext;
-                    mVolumePanel = new VolumePanel(context, AudioService.this);
+                    mVolumePanel = new VolumePanel(context, mContext, AudioService.this);
                     mVolumePanel.postMasterVolumeChanged(flags);
                 }
             });
@@ -4616,7 +4616,7 @@ public class AudioService extends IAudioService.Stub {
                     }
 
                     final Context context = mUiContext != null ? mUiContext : mContext;
-                    mVolumePanel = new VolumePanel(context, AudioService.this);
+                    mVolumePanel = new VolumePanel(context, mContext, AudioService.this);
                     mVolumePanel.postMasterMuteChanged(flags);
                 }
             });
@@ -4635,7 +4635,7 @@ public class AudioService extends IAudioService.Stub {
                     }
 
                     final Context context = mUiContext != null ? mUiContext : mContext;
-                    mVolumePanel = new VolumePanel(context, AudioService.this);
+                    mVolumePanel = new VolumePanel(context, mContext, AudioService.this);
                     mVolumePanel.postVolumeChanged(streamType, flags);
                 }
             });
@@ -4654,7 +4654,7 @@ public class AudioService extends IAudioService.Stub {
                     }
 
                     final Context context = mUiContext != null ? mUiContext : mContext;
-                    mVolumePanel = new VolumePanel(context, AudioService.this);
+                    mVolumePanel = new VolumePanel(context, mContext, AudioService.this);
                     mVolumePanel.postDisplaySafeVolumeWarning(flags);
                 }
             });

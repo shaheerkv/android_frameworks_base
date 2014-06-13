629076a5de8302cb0c3594d69e0281d47034a900
Raj Yengisetty <rajesh@cyngn.com>
2014-06-12 17:52:22 -0700

    Add the appropriate @hide flag so we can build!
    Change-Id: Id45f682287741badb13e256f6029c52820a068bd

--- core/java/android/content/pm/ApplicationInfo.java
+++ core/java/android/content/pm/ApplicationInfo.java
@@ -478,6 +478,7 @@ public class ApplicationInfo extends PackageItemInfo implements Parcelable {
     /**
      * When true, indicates that any one component within this application is
      * protected.
+     * @hide
      */
     public boolean protect = false;
     
--- test-runner/src/android/test/mock/MockPackageManager.java
+++ test-runner/src/android/test/mock/MockPackageManager.java
@@ -647,6 +647,9 @@ public class MockPackageManager extends PackageManager {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * @hide
+     */
     @Override
     public void setComponentProtectedSetting(ComponentName componentName, boolean newState) {
         throw new UnsupportedOperationException();

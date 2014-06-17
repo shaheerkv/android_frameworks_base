c75ff3a0e9cc6123e9291fde0ac3b0778f27edb5
Schubi <schubi@erlangen.ccc.de>
2014-06-17 11:12:29 +0200

    HOVER: Add QS Tile [1/2]
    Change-Id: I73b41f80762cb71fa86630314891e9670b43ea87
    
    Conflicts:
    	core/java/com/android/internal/util/liquid/QSConstants.java
    	packages/SystemUI/res/values/liquid_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -59,6 +59,7 @@ import static com.android.internal.util.liquid.QSConstants.TILE_REBOOT;
 import static com.android.internal.util.liquid.QSConstants.TILE_ONTHEGO;
 import static com.android.internal.util.liquid.QSConstants.TILE_FCHARGE;
 import static com.android.internal.util.liquid.QSConstants.TILE_PROFILE;
+import static com.android.internal.util.liquid.QSConstants.TILE_HOVER;
 
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
@@ -117,6 +118,7 @@ import com.android.systemui.quicksettings.RebootTile;
 import com.android.systemui.quicksettings.OnTheGoTile;
 import com.android.systemui.quicksettings.FastChargeTile;
 import com.android.systemui.quicksettings.ProfileTile;
+import com.android.systemui.quicksettings.HoverTile;
 
 
 import java.util.ArrayList;
@@ -280,6 +282,8 @@ public class QuickSettingsController {
                 qs = new BatterySaverTile(mContext, this);
             } else if (tile.equals(TILE_PROFILE)) {
                 qs = new ProfileTile(mContext, this);
+            } else if (tile.equals(TILE_HOVER)) {
+                qs = new HoverTile(mContext, this);
             }
 
             if (qs != null) {

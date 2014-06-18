36dd9d36684b1e0afd809344dd08c44d80c2fbcf
Clark Scheff <clark@cyngn.com>
2014-06-18 16:39:16 -0700

    Use systemui's applied theme for notifications.
    Notifications contain RemoteViews which are inflated using the
    application's context for which this notification belongs to.  This
    can look out of place if SystemUI is using a different theme than the
    rest of the system.
    
    This patch will use SystemUIs theme when inflating the RemoteViews,
    giving us a more consistent look in the notification drawer.
    
    Change-Id: I9514ce7fcc4858bad3d3c4190f55c1f5a1441d7c
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/StatusBarIconView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/StatusBarIconView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/StatusBarIconView.java
@@ -20,12 +20,13 @@ import android.app.Notification;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.content.res.ThemeConfig;
+import android.database.ContentObserver;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
-import android.database.ContentObserver;
 import android.os.Handler;
 import android.os.UserHandle;
 import android.provider.Settings;
@@ -192,8 +193,10 @@ public class StatusBarIconView extends AnimatedImageView {
                 if (userId == UserHandle.USER_ALL) {
                     userId = UserHandle.USER_OWNER;
                 }
-                r = context.getPackageManager()
-                        .getResourcesForApplicationAsUser(icon.iconPackage, userId);
+                PackageManager pm = context.getPackageManager();
+                final ThemeConfig config = context.getResources().getConfiguration().themeConfig;
+                final String pkgName = config.getOverlayPkgNameForApp(context.getPackageName());
+                r = pm.getThemedResourcesForApplicationAsUser(icon.iconPackage, pkgName, userId);
             } catch (PackageManager.NameNotFoundException ex) {
                 Log.e(TAG, "Icon package not found: " + icon.iconPackage);
                 return null;

TheKang/frameworks_base

Framework: Rewrite Quiet Hours (2/3)
Added:
	New Toggle States.
		Toggle is now aware
		of if quiet hours
		is truly enabled or not.
		Enable "force" mode
		on click: turns quiet hours
		on / off
		Double-click to return
		to "timed" mode.

	New Features:
		Wifi Connected Requirement
		Battery Charging Requirement
		Individual day times

        Completely rewrote quiet hours
                to reduce system calls.
                No more calendar objects
                or checking three preferences
                and comparing their values
                on each quiet hours check.
                Reduce that to ONE preference
                check per preference

PS2: Ensure booted before firing intent

Change-Id: I61ef5afa32c5ff883129b9362423af7f724c5ac1

Conflicts:
	packages/SystemUI/src/com/android/systemui/quicksettings/QuietHoursTile.java
	services/java/com/android/server/NotificationManagerService.java

packages/SystemUI/src/com/android/systemui/power/PowerUI.java
@@ -56,6 +56,9 @@ public class PowerUI extends SystemUI {
 
     static final boolean DEBUG = false;
 
+    private static final String UPDATE_QUIET_HOURS_MODES =
+            "com.android.settings.slim.service.UPDATE_QUIET_HOURS_MODES";
+
     Handler mHandler = new Handler();
 
     int mBatteryLevel = 100;
@@ -283,6 +286,22 @@ public class PowerUI extends SystemUI {
                     || action.equals(Intent.ACTION_POWER_DISCONNECTED)) {
                 final ContentResolver cr = mContext.getContentResolver();
 
+                final int quietHoursCharge = Settings.System.getInt(cr,
+                        Settings.System.QUIET_HOURS_REQUIRE_CHARGING, 0);
+                if (quietHoursCharge != 0 &&
+                        action.equals(Intent.ACTION_POWER_CONNECTED)) {
+                    Settings.System.putInt(cr,
+                            Settings.System.QUIET_HOURS_REQUIRE_CHARGING, 2);
+                    sendUpdateIntent();
+                } else if (quietHoursCharge != 0 &&
+                        action.equals(Intent.ACTION_POWER_DISCONNECTED)){
+                    if (!mIgnoreFirstPowerEvent) {
+                        Settings.System.putInt(cr,
+                                Settings.System.QUIET_HOURS_REQUIRE_CHARGING, 1);
+                        sendUpdateIntent();
+                    }
+                }
+
                 if (mIgnoreFirstPowerEvent) {
                     mIgnoreFirstPowerEvent = false;
                 } else {
@@ -296,6 +315,12 @@ public class PowerUI extends SystemUI {
             }
         }
     };
+
+    private void sendUpdateIntent() {
+        Intent intent = new Intent();
+        intent.setAction(UPDATE_QUIET_HOURS_MODES);
+        mContext.sendBroadcast(intent);
+    }
 
     void dismissLowBatteryWarning() {
         if (mLowBatteryDialog != null) {

packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
@@ -64,6 +64,9 @@ public class NetworkController extends BroadcastReceiver implements DemoMode {
 
     private static final int FLIGHT_MODE_ICON = R.drawable.stat_sys_signal_flightmode;
 
+    private static final String UPDATE_QUIET_HOURS_MODES =
+            "com.android.settings.slim.service.UPDATE_QUIET_HOURS_MODES";
+
     // telephony
     boolean mHspaDataDistinguishable;
     final TelephonyManager mPhone;
@@ -858,6 +861,7 @@ public class NetworkController extends BroadcastReceiver implements DemoMode {
             mWifiConnected = networkInfo != null && networkInfo.isConnected();
             // If we just connected, grab the inintial signal strength and ssid
             if (mWifiConnected && !wasConnected) {
+                updateQuietHoursState();
                 // try getting it out of the intent first
                 WifiInfo info = (WifiInfo) intent.getParcelableExtra(WifiManager.EXTRA_WIFI_INFO);
                 if (info == null) {
@@ -869,6 +873,7 @@ public class NetworkController extends BroadcastReceiver implements DemoMode {
                     mWifiSsid = null;
                 }
             } else if (!mWifiConnected) {
+                updateQuietHoursState();
                 mWifiSsid = null;
             }
         } else if (action.equals(WifiManager.RSSI_CHANGED_ACTION)) {
@@ -878,6 +883,28 @@ public class NetworkController extends BroadcastReceiver implements DemoMode {
         }
 
         updateWifiIcons();
+    }
+
+    private void updateQuietHoursState() {
+        final int quietHoursWiFi = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.QUIET_HOURS_REQUIRE_WIFI, 0);
+        if (quietHoursWiFi == 1 && mWifiConnected) {
+            // We just updated and need to inform quiet hours that we're connected
+            Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.QUIET_HOURS_REQUIRE_WIFI, 2);
+            sendUpdateIntent();
+        } else if (quietHoursWiFi == 2 && !mWifiConnected) {
+            // We just updated and need to inform quiet hours that we're not connected
+            Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.QUIET_HOURS_REQUIRE_WIFI, 1);
+            sendUpdateIntent();
+        }
+    }
+
+    private void sendUpdateIntent() {
+        Intent intent = new Intent();
+        intent.setAction(UPDATE_QUIET_HOURS_MODES);
+        mContext.sendBroadcast(intent);
     }
 
     private void updateWifiIcons() {

services/java/com/android/server/BatteryService.java
@@ -18,7 +18,6 @@ package com.android.server;
 
 import android.os.BatteryStats;
 import com.android.internal.app.IBatteryStats;
-import com.android.internal.util.slim.QuietHoursHelper;
 import com.android.server.am.BatteryStatsService;
 
 import android.app.ActivityManagerNative;
@@ -99,6 +98,9 @@ public final class BatteryService extends Binder {
 
     private static final String DUMPSYS_DATA_PATH = "/data/system/";
 
+    private static final String UPDATE_QUIET_HOURS_MODES =
+            "com.android.settings.slim.service.UPDATE_QUIET_HOURS_MODES";
+
     // This should probably be exposed in the API, though it's not critical
     private static final int BATTERY_PLUGGED_NONE = 0;
 
@@ -113,6 +115,7 @@ public final class BatteryService extends Binder {
     private int mLastBatteryStatus;
     private int mLastBatteryHealth;
     private boolean mLastBatteryPresent;
+    private int mQuietHoursMode;
     private int mLastBatteryLevel;
     private int mLastBatteryVoltage;
     private int mLastBatteryTemperature;
@@ -744,7 +747,10 @@ public final class BatteryService extends Binder {
             if (!mLightEnabled) {
                 // No lights if explicitly disabled
                 mBatteryLight.turnOff();
-            } else if (QuietHoursHelper.inQuietHours(mContext, Settings.System.QUIET_HOURS_DIM)) {
+            } else if (Settings.System.getIntForUser(
+                    mContext.getContentResolver(),
+                    Settings.System.QUIET_HOURS_DIM,
+                    0, UserHandle.USER_CURRENT_OR_SELF) == 2) {
                 if (mLedPulseEnabled && level < mLowBatteryWarningLevel &&
                         status != BatteryManager.BATTERY_STATUS_CHARGING) {
                     // The battery is low, the device is not charging and the low battery pulse
@@ -826,12 +832,6 @@ public final class BatteryService extends Binder {
                     Settings.System.QUIET_HOURS_ENABLED), false, this,
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.QUIET_HOURS_START), false, this,
-                    UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.QUIET_HOURS_END), false, this,
-                    UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.QUIET_HOURS_DIM), false, this,
                     UserHandle.USER_ALL);
 
@@ -845,6 +845,17 @@ public final class BatteryService extends Binder {
         public void update() {
             ContentResolver resolver = mContext.getContentResolver();
             Resources res = mContext.getResources();
+
+            final int newQhMode = Settings.System.getIntForUser(resolver,
+                    Settings.System.QUIET_HOURS_ENABLED, 0,
+                    UserHandle.USER_CURRENT_OR_SELF);
+            if (newQhMode != mQuietHoursMode
+                    && ActivityManagerNative.isSystemReady()) {
+                Intent intent = new Intent();
+                intent.setAction(UPDATE_QUIET_HOURS_MODES);
+                mContext.sendBroadcast(intent);
+            }
+            mQuietHoursMode = newQhMode;
 
             // Battery light enabled
             mLightEnabled = Settings.System.getIntForUser(resolver,

shaheerkv/android_frameworks_base

	core/java/com/android/internal/util/liquid/QuietHoursHelper.java
	packages/SystemUI/src/com/android/systemui/ReminderReceiver.java
	services/java/com/android/server/BatteryService.java
	services/java/com/android/server/VibratorService.java

packages/SystemUI/src/com/android/systemui/power/PowerUI.java
@@ -56,6 +56,9 @@ public class PowerUI extends SystemUI {
 
     static final boolean DEBUG = false;
 
+    private static final String UPDATE_QUIET_HOURS_MODES =
+            "com.android.settings.liquid.service.UPDATE_QUIET_HOURS_MODES";
+
     Handler mHandler = new Handler();
 
     int mBatteryLevel = 100;
@@ -283,6 +286,22 @@ public class PowerUI extends SystemUI {
                     || action.equals(Intent.ACTION_POWER_DISCONNECTED)) {
                 final ContentResolver cr = mContext.getContentResolver();
 
+                final int quietHoursCharge = Settings.System.getInt(cr,
+                        Settings.System.QUIET_HOURS_REQUIRE_CHARGING, 0);
+                if (quietHoursCharge != 0 &&
+                        action.equals(Intent.ACTION_POWER_CONNECTED)) {
+                    Settings.System.putInt(cr,
+                            Settings.System.QUIET_HOURS_REQUIRE_CHARGING, 2);
+                    sendUpdateIntent();
+                } else if (quietHoursCharge != 0 &&
+                        action.equals(Intent.ACTION_POWER_DISCONNECTED)){
+                    if (!mIgnoreFirstPowerEvent) {
+                        Settings.System.putInt(cr,
+                                Settings.System.QUIET_HOURS_REQUIRE_CHARGING, 1);
+                        sendUpdateIntent();
+                    }
+                }
+
                 if (mIgnoreFirstPowerEvent) {
                     mIgnoreFirstPowerEvent = false;
                 } else {
@@ -296,6 +315,12 @@ public class PowerUI extends SystemUI {
             }
         }
     };
+
+    private void sendUpdateIntent() {
+        Intent intent = new Intent();
+        intent.setAction(UPDATE_QUIET_HOURS_MODES);
+        mContext.sendBroadcast(intent);
+    }
 
     void dismissLowBatteryWarning() {
         if (mLowBatteryDialog != null) {

packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
@@ -63,6 +63,9 @@ public class NetworkController extends BroadcastReceiver implements DemoMode {
 
     private static final int FLIGHT_MODE_ICON = R.drawable.stat_sys_signal_flightmode;
 
+    private static final String UPDATE_QUIET_HOURS_MODES =
+            "com.android.settings.liquid.service.UPDATE_QUIET_HOURS_MODES";
+
     // telephony
     boolean mHspaDataDistinguishable;
     final TelephonyManager mPhone;
@@ -907,6 +910,7 @@ public class NetworkController extends BroadcastReceiver implements DemoMode {
             mWifiConnected = networkInfo != null && networkInfo.isConnected();
             // If we just connected, grab the inintial signal strength and ssid
             if (mWifiConnected && !wasConnected) {
+                updateQuietHoursState();
                 // try getting it out of the intent first
                 WifiInfo info = (WifiInfo) intent.getParcelableExtra(WifiManager.EXTRA_WIFI_INFO);
                 if (info == null) {
@@ -918,6 +922,7 @@ public class NetworkController extends BroadcastReceiver implements DemoMode {
                     mWifiSsid = null;
                 }
             } else if (!mWifiConnected) {
+                updateQuietHoursState();
                 mWifiSsid = null;
             }
         } else if (action.equals(WifiManager.RSSI_CHANGED_ACTION)) {
@@ -927,6 +932,28 @@ public class NetworkController extends BroadcastReceiver implements DemoMode {
         }
 
         updateWifiIcons();
+    }
+
+    private void updateQuietHoursState() {
+        final int quietHoursWiFi = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.QUIET_HOURS_REQUIRE_WIFI, 0);
+        if (quietHoursWiFi == 1 && mWifiConnected) {
+            // We just updated and need to inform quiet hours that we're connected
+            Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.QUIET_HOURS_REQUIRE_WIFI, 2);
+            sendUpdateIntent();
+        } else if (quietHoursWiFi == 2 && !mWifiConnected) {
+            // We just updated and need to inform quiet hours that we're not connected
+            Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.QUIET_HOURS_REQUIRE_WIFI, 1);
+            sendUpdateIntent();
+        }
+    }
+
+    private void sendUpdateIntent() {
+        Intent intent = new Intent();
+        intent.setAction(UPDATE_QUIET_HOURS_MODES);
+        mContext.sendBroadcast(intent);
     }
 
     private void updateWifiIcons() {

services/java/com/android/server/BatteryService.java
@@ -18,7 +18,6 @@ package com.android.server;
 
 import android.os.BatteryStats;
 import com.android.internal.app.IBatteryStats;
-import com.android.internal.util.liquid.QuietHoursHelper;
 import com.android.server.am.BatteryStatsService;
 
 import android.app.ActivityManagerNative;
@@ -99,6 +98,9 @@ public final class BatteryService extends Binder {
 
     private static final String DUMPSYS_DATA_PATH = "/data/system/";
 
+    private static final String UPDATE_QUIET_HOURS_MODES =
+            "com.android.settings.liquid.service.UPDATE_QUIET_HOURS_MODES";
+
     // This should probably be exposed in the API, though it's not critical
     private static final int BATTERY_PLUGGED_NONE = 0;
 
@@ -113,6 +115,7 @@ public final class BatteryService extends Binder {
     private int mLastBatteryStatus;
     private int mLastBatteryHealth;
     private boolean mLastBatteryPresent;
+    private int mQuietHoursMode;
     private int mLastBatteryLevel;
     private int mLastBatteryVoltage;
     private int mLastBatteryTemperature;
@@ -744,7 +747,10 @@ public final class BatteryService extends Binder {
             if (!mLightEnabled) {
                 // No lights if explicitly disabled
                 mBatteryLight.turnOff();
-            } else if (QuietHoursHelper.inQuietHours(mContext, Settings.System.QUIET_HOURS_DIM)) {
+            } else if (Settings.System.getIntForUser(
+                    mContext.getContentResolver(),
+                    Settings.System.QUIET_HOURS_DIM,
+                    0, UserHandle.USER_CURRENT_OR_SELF) == 2) {
                 if (mLedPulseEnabled && level < mLowBatteryWarningLevel &&
                         status != BatteryManager.BATTERY_STATUS_CHARGING) {
                     // The battery is low, the device is not charging and the low battery pulse
@@ -826,12 +832,6 @@ public final class BatteryService extends Binder {
                     Settings.System.QUIET_HOURS_ENABLED), false, this,
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.QUIET_HOURS_START), false, this,
-                    UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.QUIET_HOURS_END), false, this,
-                    UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.QUIET_HOURS_DIM), false, this,
                     UserHandle.USER_ALL);
 
@@ -845,6 +845,17 @@ public final class BatteryService extends Binder {
         public void update() {
             ContentResolver resolver = mContext.getContentResolver();
             Resources res = mContext.getResources();
+
+            final int newQhMode = Settings.System.getIntForUser(resolver,
+                    Settings.System.QUIET_HOURS_ENABLED, 0,
+                    UserHandle.USER_CURRENT_OR_SELF);
+            if (newQhMode != mQuietHoursMode
+                    && ActivityManagerNative.isSystemReady()) {
+                Intent intent = new Intent();
+                intent.setAction(UPDATE_QUIET_HOURS_MODES);
+                mContext.sendBroadcast(intent);
+            }
+            mQuietHoursMode = newQhMode;
 
             // Battery light enabled
             mLightEnabled = Settings.System.getIntForUser(resolver,
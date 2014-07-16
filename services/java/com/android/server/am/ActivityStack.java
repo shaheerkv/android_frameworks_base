de0ff75a4ba92f856e31b3a2e9f3bec639cf757c
lion0738 <lion0738@naver.com>
2014-07-16 23:11:32 +0900

    Frameworks: Remove option to pause underlying app (1/2)
    Change-Id: I9b296038576fbacec70841f1b20b875d3937acf2

--- services/java/com/android/server/am/ActivityStack.java
+++ services/java/com/android/server/am/ActivityStack.java
@@ -73,7 +73,6 @@ import android.os.RemoteException;
 import android.os.SystemClock;
 import android.os.Trace;
 import android.os.UserHandle;
-import android.provider.Settings;
 import android.util.EventLog;
 import android.util.Slog;
 import android.view.ContextThemeWrapper;
@@ -1388,7 +1387,7 @@ final class ActivityStack {
         // We need to start pausing the current activity so the top one
         // can be resumed...
         boolean pausing = mStackSupervisor.pauseBackStacks(userLeaving);
-        if (mResumedActivity != null && (pauseActiveAppWhenUsingHalo() || !next.floatingWindow)) {
+        if (mResumedActivity != null && !next.floatingWindow) {
             pausing = true;
             startPausingLocked(userLeaving, false);
             if (DEBUG_STATES) Slog.d(TAG, "resumeTopActivityLocked: Pausing " + mResumedActivity);
@@ -1664,12 +1663,6 @@ final class ActivityStack {
 
         if (DEBUG_STACK) mStackSupervisor.validateTopActivitiesLocked();
         return true;
-    }
-
-    private boolean pauseActiveAppWhenUsingHalo() {
-        int isLowRAM = (!ActivityManager.isLowRamDeviceStatic()) ? 0 : 1;
-        return Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.HALO_PAUSE, isLowRAM) == 1;
     }
 
     private void insertTaskAtTop(TaskRecord task) {

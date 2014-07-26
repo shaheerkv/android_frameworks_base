f76deef3173f38d4ce68c57935932dcff780a756
cristianomatos <cristianobmatos@gmail.com>
2014-07-26 19:46:41 -0300

    Workaround to update PIE resources on theme changes without need to reboot or eneble/disable
    - Here we dettach and attach PIE to simulate disable/enable action
    
    Change-Id: I201178f35b8baafeb1608536e7e3b517662330e9

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -4593,6 +4593,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         updateExpandedViewPos(EXPANDED_LEAVE_ALONE);
 
+        // Workaround to update drawable
+        // resources of pie on theme changes
+        attachPieContainer(!isPieEnabled());
+        attachPieContainer(isPieEnabled());
+
         restorePieTriggerMask();
 
         checkBarModes();

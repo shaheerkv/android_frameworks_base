704dd3d26f1e7e1418c7020d513c5e5e21ed0765
Lokesh Chamane <lokesh.c703@gmail.com>
2014-08-09 17:13:40 -0400

    SlimPIE: Configurable sensitivity [1/2]
    Signed-off-by: Lokesh Chamane <lokesh.c703@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Change-Id: Ia6ebe8ab40b677b268170a7006de5fc7c79988b8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3314,6 +3314,12 @@ public final class Settings {
                 "pie_buttons_config_second_layer";
 
         /**
+         * Pie trigger sensitivity
+         * @hide
+         */
+        public static final String PIE_TRIGGER_SENSITIVITY = "pie_trigger_sensitivity";
+
+        /**
          * QuickSettings configuration
          * @hide
          */

1091edf434669946651f8bf4ffff5b423eca0e5b
liquid0624 <rjbirnesser@gmail.com>
2014-08-13 20:16:25 -0400

    Revert "Add Weather Status Bar Header (1/4)"
    This reverts commit be2bc404e3bc6572123999f7b1e908e347abea7f.
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    
    Change-Id: I8fd95a914d444c40d6039f33291a1e30082b0107

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -303,9 +303,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private TextView mEmergencyCallLabel;
     private int mNotificationHeaderHeight;
 
-    private View mWeatherHeader;
-    private boolean mWeatherEnabled = false;
-
     // Notification reminder
     private View mReminderHeader;
     private ImageView mSpacer;
@@ -590,15 +587,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     Settings.System.HEADS_UP_SHOW_UPDATE), false, this,
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.SYSTEMUI_WEATHER_HEADER_VIEW), false, this,
-                    UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.SYSTEMUI_WEATHER_NOTIFICATION), false, this,
-                    UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.SYSTEMUI_WEATHER_ICON), false, this,
-                    UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.IME_CURSOR_LONGPRESS_ACTION),
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
@@ -855,13 +843,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 enableOrDisableReminder();
             }
 
-            boolean weatherHolder = Settings.System.getBoolean(resolver,
-                Settings.System.SYSTEMUI_WEATHER_HEADER_VIEW, false);
-            if (weatherHolder != mWeatherEnabled) {
-                mWeatherEnabled = weatherHolder;
-                enableOrDisableWeather();
-            }
-
             mShowStatusBarCarrier = Settings.System.getInt(
                     resolver, Settings.System.STATUS_BAR_CARRIER, 0) == 1;
                     showStatusBarCarrierLabel(mShowStatusBarCarrier);
@@ -959,16 +940,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mReminderPortraitWidth = mReminderLandscapeWidth;
         }
         enableOrDisableReminder();
-    }
-
-    private void enableOrDisableWeather() {
-        if (mWeatherEnabled) {
-            mWeatherHeader.setVisibility(View.VISIBLE);
-            mWeatherHeader.setEnabled(true);
-        } else {
-            mWeatherHeader.setVisibility(View.GONE);
-            mWeatherHeader.setEnabled(false);
-        }
     }
 
     private ArrayList<String>splitString(String message, int maxWidth) {
@@ -1347,8 +1318,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         mNotificationPanelHeader = mStatusBarWindow.findViewById(R.id.header);
 
-        mWeatherHeader = mStatusBarWindow.findViewById(R.id.weather_text);
-
         mReminderHeader = mStatusBarWindow.findViewById(R.id.reminder_header);
         mReminderHeader.setOnClickListener(mReminderButtonListener);
         mReminderHeader.setOnLongClickListener(mReminderLongButtonListener);
@@ -1367,9 +1336,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         mReminderEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
                     Settings.System.REMINDER_ALERT_ENABLED, 0, UserHandle.USER_CURRENT) != 0;
-
-        mWeatherEnabled = Settings.System.getBoolean(mContext.getContentResolver(),
-                    Settings.System.SYSTEMUI_WEATHER_HEADER_VIEW, false);
 
         View view = View.inflate(mContext, R.layout.reminder_entry, null);
         mTextHolder = (TextView) view.findViewById(R.id.message_content);
